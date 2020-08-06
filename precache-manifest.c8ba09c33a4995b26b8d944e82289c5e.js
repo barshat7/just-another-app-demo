52522255ec87929925b576307a0e65f5ab226959
barshat7 <barshatrai99@gmail.com>
2020-08-06 23:19:07 +0530

    Updates

---
+++ precache-manifest.c8ba09c33a4995b26b8d944e82289c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd376458cfbd8765f743c316bff096de",
+    "url": "/just-another-app-demo/index.html"
+  },
+  {
+    "revision": "88093289c9ec8e9fc7b1",
+    "url": "/just-another-app-demo/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "61a46f5847e8f8307a1a",
+    "url": "/just-another-app-demo/static/js/2.dd6ef1fd.chunk.js"
+  },
+  {
+    "revision": "207eac4733210009b985327aa056650b",
+    "url": "/just-another-app-demo/static/js/2.dd6ef1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88093289c9ec8e9fc7b1",
+    "url": "/just-another-app-demo/static/js/main.378f8bd9.chunk.js"
+  },
+  {
+    "revision": "27df8bb1e0688e106bdf",
+    "url": "/just-another-app-demo/static/js/runtime-main.f47f9fbc.js"
+  }
+]);
